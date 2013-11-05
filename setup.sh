2a7a4f3aca371626b8da7a5dd93e8e6e2d0a04ab
Jeff Borowitz <jborowitz@gmail.com>
2013-11-05 00:41:34 +0000

    Improve setup to start mongod and restore db

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 wget https://s3.amazonaws.com/mongodb-enron-email/enron_mongo.tar.bz2
 tar jxf enron_mongo.tar.bz2
+nohup mongod --dbpath /mnt/enron/dump/ &
+#mongod --dbpath /mnt/enron/dump/
 mongorestore dump
