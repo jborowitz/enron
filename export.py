95afd72b46d475aaed37e8c512105b2474fc18a0
Jeffrey Borowitz <jeffrey.borowitz@giantoak.com>
2014-03-08 22:47:19 -0500

    Add excel export

--- export.py
+++ export.py
@@ -1,5 +1,6 @@
 import pymongo
 import json
+import pandas
 client = pymongo.MongoClient()
 enron = client.enron_mail
 # use the enron_mail database
@@ -16,12 +17,38 @@ for email in b:
     myfile.write(json.dumps(email)+'\n')
 myfile.close()
 
+datas = set([])
 email_file = open('emails.json','r')
 with open('emails.json') as email_file:
     for line in email_file:
         data = json.loads(line)
+        headers = data.pop('headers')
+        for f in data.keys():
+            datas.add(f)
+        for f in headers.keys():
+            datas.add(f)
         # Within this loop, we are reading in one line at a time.
         # This will avoid having all 500k emails in memory
 
-# If you wanted all the data in memory, you could do:
-# emails = [json.loads(line) for line in open('emails.json').readlines()]
+
+# The code below writes the loaded data to a spreadsheet, replacing
+# carriage returns with the string [newline]
+written = False
+a = pandas.DataFrame(index=[0], columns=datas)
+with open('emails.json') as email_file:
+    for line in email_file:
+        mail = json.loads(line)
+        for f in data.keys():
+	    a.at[0,f] = mail[f].replace('\n','[newline]')
+        for f in headers.keys():
+            try:
+                a.at[0,f] = mail['headers'][f].replace('\n','[newline]')
+            except:
+                continue
+        if not written:
+            a.to_csv('file.csv', index=False, encoding='utf-8')
+            written=True
+        else:
+            a.to_csv('file.csv', index=False, mode='a', header=False, encoding='utf-8')
+
+
